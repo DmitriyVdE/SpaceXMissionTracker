59e11b72166a59f6205ccb54a8ed0b0b2043899e
Dmitriy Van der Elst <dmitriy.vde@gmail.com>
2020-04-20 17:09:54 +0200

    Webpack is gone, GONE I TELL YOU!

--- API/src/index.js
+++ API/src/index.js
@@ -1,11 +1,11 @@
-const debug = require('debug')('server:debug');
+const debug =require('debug')('server:debug');
 import mongoose from 'mongoose';
 import config from 'config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import userRouter from './routes/user';
 
-mongoose.connect(config.get('mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_URL + '?retryWrites=true&w=majority'), {
+mongoose.connect(config.get('database'), {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -23,7 +23,7 @@ const app = express();
  app.use(bodyParser.json());
  app.use('/api/v1/', userRouter);
 
-const listen = app.listen(config.get('port'),() => {
+const listen =app.listen(config.get('port'),()=>{
     debug(`server is running on port ${config.get('port')} and in ${config.get('name')} mode`);
     console.log(`server is running on port ${config.get('port')} and in ${config.get('name')} mode`);
 })
